75abf739cc150111ed2413bc9522ec32c1029468
Emily Sher <emilysher@Emilys-MacBook-Pro-78.local>
2025-12-15 12:40:37 -0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from "react-router-dom";
+import PhoneCanvas from "./components/PhoneCanvas";
 
 import Swipe from "./screens/Swipe";
 import Match from "./screens/Match";
 import Closet from "./screens/Closet";
 
-function App() {
+function Layout() {
+  return (
+    <PhoneCanvas>
+      <Outlet />
+    </PhoneCanvas>
+  );
+}
+
+export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Swipe />} />
-        <Route path="/match" element={<Match />} />
-        <Route path="/closet" element={<Closet />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/swipe" replace />} />
+
+          <Route path="/swipe" element={<Swipe />} />
+          <Route path="/match" element={<Match />} />
+          <Route path="/closet" element={<Closet />} />
+        </Route>
       </Routes>
     </Router>
   );
 }
-
-export default App;
